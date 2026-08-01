07df74ac91c5f349ddeef99529baa79ad4a676b9
Shayan NasrAbadi <williams.ca001@gmail.com>
2026-08-01 12:45:05 +0330

    SMS system added

---
+++ src/utils/sms.js
@@ -0,0 +1,41 @@
+const send = (code, phone, args) => {
+
+const https = require('https');
+  
+  const data = JSON.stringify({
+      'bodyId': code,
+      'to': phone,
+      'args': args
+  });
+  
+  const options = {
+      hostname: 'console.melipayamak.com',
+      port: 443,
+      path: process.env.smsAPI,
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': data.length
+      }
+  };
+  
+  const req = https.request(options, res => {
+      console.log('statusCode: ' + res.statusCode);
+  
+      res.on('data', d => {
+          process.stdout.write(d)
+      });
+  });
+  
+  req.on('error', error => {
+      console.error(error);
+  });
+  
+  req.write(data);
+  req.end();
+
+}
+
+module.exports = {
+    send
+}
