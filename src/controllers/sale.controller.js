057127ac0d2a0a3ca11509921d64998e64a89869
Shayan NasrAbadi <williams.ca001@gmail.com>
2026-09-21 20:37:56 +0330

    sms bug fix

--- src/controllers/sale.controller.js
+++ src/controllers/sale.controller.js
@@ -104,7 +104,7 @@ exports.create = async (req, res, next) => {
     });
 
     if(sms && sms == true){
-      smsUtil.send(process.env.FAKTOR_SMS_CODE, resolved.phone + "", [fmtDate(sale.date), sale.qty + " " + sale.unit + "", sale.total + " تومان", sale.paid_amount||0 + ' تومان', sale.paid_amount >= 1 ? (sale.total - sale.paid_amount) : 0 + ' تومان'])
+      smsUtil.send(process.env.FAKTOR_SMS_CODE, resolved.phone + "", [fmtDate(sale.date), sale.qty + " " + sale.unit + "", sale.total + " تومان", paidAmount.toString()||0 + ' تومان', paidAmount >= 1 ? (sale.total - paidAmount) : 0 + ' تومان'])
     }
 
     const full = await Sale.findByPk(sale.id, { include: [{ model: Payment, as: "payments" }] });
